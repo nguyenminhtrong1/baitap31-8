2e8bfc7caa4c2c74a36e084b7bf01b6c8d06b30c
NGUYENMINHTRONG <77635440+nguyenminhtrong1@users.noreply.github.com>
2021-08-31 11:20:57 +0700

    bt

--- baitap31-8/Controllers/DemoController.cs
+++ baitap31-8/Controllers/DemoController.cs
@@ -14,10 +14,11 @@ namespace baitap31_8.Controllers
         }
         public ActionResult Timx(string soA, string soB)
         {
-            double so1 = Convert.ToDouble;
-            double so2 = Convert.ToDouble;
+            double so1 = Convert.ToDouble(soA);
+            double so2 = Convert.ToDouble(soB);
             double gtx = so2 / so1;
             ViewBag.timgtx = gtx;
+            
             return View();
         }
     }
