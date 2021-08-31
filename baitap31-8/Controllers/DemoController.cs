3a95c03567f691ca368eb8a4b9431a9221e4b183
NGUYENMINHTRONG <77635440+nguyenminhtrong1@users.noreply.github.com>
2021-08-31 11:11:52 +0700

    a

---
+++ baitap31-8/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace baitap31_8.Controllers
+{
+    public class DemoController : Controller
+    {
+        public ActionResult Timx()
+        {
+            return View();
+        }
+        public ActionResult Timx(string soA, string soB)
+        {
+            double so1 = Convert.ToDouble;
+            double so2 = Convert.ToDouble;
+            double gtx = so2 / so1;
+            ViewBag.timgtx = gtx;
+            return View();
+        }
+    }
+}
