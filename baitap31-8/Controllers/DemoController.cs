8cc798ec2b42b652ecdf4f1d8784de20258e4112
NGUYENMINHTRONG <77635440+nguyenminhtrong1@users.noreply.github.com>
2021-08-31 13:04:42 +0700

    bt

--- baitap31-8/Controllers/DemoController.cs
+++ baitap31-8/Controllers/DemoController.cs
@@ -12,7 +12,7 @@ namespace baitap31_8.Controllers
         {
             return View();
         }
-        public ActionResult Timgiatrix(double soA, double soB)
+        public ActionResult Timgiatrix(string soA, string soB)
         {
             double so1 = Convert.ToDouble(soA);
             double so2 = Convert.ToDouble(soB);
