8867560284c9377757c863ee1e3b79117729c8be
NGUYENMINHTRONG <77635440+nguyenminhtrong1@users.noreply.github.com>
2021-08-31 10:53:56 +0700

    a

---
+++ baitap31-8/Models/giaiPTbac1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace baitap31_8.Models
+{
+    public class giaiPTbac1
+    {
+        public double Timx( double a, double b)
+        {
+            double giatrix = 0;
+            giatrix = b / a;
+            return giatrix;
+        }
+    }
+}
